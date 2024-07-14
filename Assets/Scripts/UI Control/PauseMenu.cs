b93eb0d693eba30481c7fdfe9d82e402636528bc
Kkenjji <156585410+Kkenjji@users.noreply.github.com>
2024-07-15 01:51:41 +0800

    Basic functionality for pause menu

--- Assets/Scripts/UI Control/PauseMenu.cs
+++ Assets/Scripts/UI Control/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -46,11 +47,6 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
-
-    }
-
-    public void QuitLevel()
-    {
-        
+        SceneManager.LoadScene("MainMenu");
     }
 }
